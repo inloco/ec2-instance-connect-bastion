3b078370d4992a127106f6d101a10dc0efca6f52
Pedro Tôrres <pedro.torres@incognia.com>
2021-10-28 13:45:33 -0300

    Initial commit
    Signed-off-by: Pedro Tôrres <pedro.torres@incognia.com>

---
+++ go.mod
@@ -0,0 +1,25 @@
+module github.com/inloco/ec2-instance-connect-bastion
+
+go 1.17
+
+require (
+	github.com/aws/aws-sdk-go-v2 v1.10.0
+	github.com/aws/aws-sdk-go-v2/config v1.9.0
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.20.0
+	github.com/aws/aws-sdk-go-v2/service/ec2instanceconnect v1.6.0
+	github.com/gliderlabs/ssh v0.3.3
+	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
+)
+
+require (
+	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.5.0 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.7.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.2.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.4.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.5.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.8.0 // indirect
+	github.com/aws/smithy-go v1.8.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+)
---
+++ main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
+	"github.com/aws/aws-sdk-go-v2/service/ec2instanceconnect"
+	"github.com/gliderlabs/ssh"
+	xssh "golang.org/x/crypto/ssh"
+)
+
+func main() {
+	awsConfig, err := config.LoadDefaultConfig(context.Background())
+	if err != nil {
+		log.Panic(err)
+	}
+
+	ec2Client := ec2.NewFromConfig(awsConfig)
+	eicClient := ec2instanceconnect.NewFromConfig(awsConfig)
+
+	server := &ssh.Server{
+		Addr: ":2222",
+		Handler: func(session ssh.Session) {
+			log.Printf("Handler(%v)\n", session)
+		},
+		PublicKeyHandler: func(ctx ssh.Context, key ssh.PublicKey) bool {
+			log.Printf("PublicKeyHandler(_, %v)\n", key)
+			return true
+		},
+		LocalPortForwardingCallback: func(ctx ssh.Context, destinationHost string, destinationPort uint32) bool {
+			log.Printf("LocalPortForwardingCallback(_, %v, %v)\n", destinationHost, destinationPort)
+
+			userInterface := ctx.Value(ssh.ContextKeyUser)
+			if userInterface == nil {
+				log.Println("ctxVal == nil")
+				return false
+			}
+
+			user, ok := userInterface.(string)
+			if !ok {
+				log.Println("!ok")
+				return false
+			}
+
+			publicKeyInterface := ctx.Value(ssh.ContextKeyPublicKey)
+			if publicKeyInterface == nil {
+				log.Println("ctxVal == nil")
+				return false
+			}
+
+			publicKey, ok := publicKeyInterface.(xssh.PublicKey)
+			if !ok {
+				log.Println("!ok")
+				return false
+			}
+			authorizedKey := xssh.MarshalAuthorizedKey(publicKey)
+
+			var availabilityZone string
+			var instanceId string
+			for _, filterName := range []string{
+				"addresses.private-ip-address",
+				"addresses.private-dns-name",
+				"association.public-ip",
+				"association.public-dns-name",
+			} {
+				out, err := ec2Client.DescribeNetworkInterfaces(ctx, &ec2.DescribeNetworkInterfacesInput{
+					Filters: []types.Filter{
+						types.Filter{
+							Name: aws.String(filterName),
+							Values: []string{
+								destinationHost,
+							},
+						},
+					},
+				})
+				if err != nil {
+					log.Println(err)
+					continue
+				}
+
+				enis := out.NetworkInterfaces
+				if len(enis) != 1 {
+					log.Println("len(enis) != 1")
+					continue
+				}
+				eni := enis[0]
+
+				az := eni.AvailabilityZone
+				if az == nil {
+					log.Println("az == nil")
+					continue
+				}
+
+				a := eni.Attachment
+				if a == nil {
+					log.Println("a == nil")
+					continue
+				}
+
+				ii := a.InstanceId
+				if ii == nil {
+					log.Println("ii == nil")
+					continue
+				}
+
+				availabilityZone = aws.ToString(az)
+				instanceId = aws.ToString(ii)
+				break
+			}
+
+			out, err := eicClient.SendSSHPublicKey(ctx, &ec2instanceconnect.SendSSHPublicKeyInput{
+				AvailabilityZone: aws.String(availabilityZone),
+				InstanceId:       aws.String(instanceId),
+				InstanceOSUser:   aws.String(user),
+				SSHPublicKey:     aws.String(string(authorizedKey)),
+			})
+			if err != nil {
+				log.Println(err)
+				return false
+			}
+			if !out.Success {
+				log.Println("!out.Success")
+				return false
+			}
+
+			return true
+		},
+		ChannelHandlers: map[string]ssh.ChannelHandler{
+			"direct-tcpip": ssh.DirectTCPIPHandler,
+			"session":      ssh.DefaultSessionHandler,
+		},
+	}
+
+	log.Println("EC2 Instance Connect Bastion")
+	log.Println(server.Addr)
+	log.Fatalln(server.ListenAndServe())
+}
